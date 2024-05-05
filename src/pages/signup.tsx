95b65f2001f77f2ca413851fa4f26c31b5146c05
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-05 18:11:39 +0600

    add placeholder

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -31,8 +31,8 @@ export default function Signup() {
             <div className="flex items-center flex-col p-4 border rounded w-full md:w-1/3 dark:border-gray-800">
                 <div className="flex flex-col w-full items-center gap-2">
                     <InputField className="w-full" placeholder="Enter email" type="email" {...register('email')} />
-                    <InputField className="w-full" type="password" {...register("password")} />
-                    <InputField className="w-full" type="password" {...register("confirmPassword")} />
+                    <InputField placeholder="Enter password" className="w-full" type="password" {...register("password")} />
+                    <InputField placeholder="Enter confirm password" className="w-full" type="password" {...register("confirmPassword")} />
                 </div>
                 {errors.email && <span>Email is required</span>}
                 {errors.password && <span>Password must be between 6 and 20 characters</span>}
