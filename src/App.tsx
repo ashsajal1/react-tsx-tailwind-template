56224eb78338183cb737e00c6e6d30344002c353
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-05 10:54:24 +0600

    setup react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+
 export default function App() {
   return (
-    <div className="bg-blue-700">App</div>
+    <RouterProvider router={router} />
   )
 }
