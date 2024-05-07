4ee217d15d8d4c7898991f01313d361420637b76
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-07 12:28:28 +0600

    make border red in dark mode if the value is invalid

--- src/components/ui/input-field.tsx
+++ src/components/ui/input-field.tsx
@@ -9,7 +9,7 @@ const InputField = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
     const btnClass = 'flex items-center justify-center gap-2 cursor-pointer select-none p-2 rounded transition duration-0 bg-primary hover:bg-primary-high text-white bg-gradient-to-br from-primary to-primary-high hover:to-primary-extra-high  active:scale-[102%] border-none'
 
     return (
-        <input {...rest} ref={ref} className={cn("p-2 border  outline-none rounded dark:text-white bg-white dark:bg-black dark:border dark:border-gray-800 focus:border-primary", className, { [btnClass]: (type === 'submit') })} title="Input field" />
+        <input {...rest} ref={ref} className={cn("p-2 border  outline-none rounded dark:text-white bg-white dark:bg-black dark:border-gray-800 focus:border-primary", className, { [btnClass]: (type === 'submit') })} title="Input field" />
     );
 });
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -30,15 +30,15 @@ export default function Signup() {
         <form className="flex items-center h-screen justify-center" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex items-center flex-col p-4 border rounded w-full md:w-1/3 dark:border-gray-800">
                 <div className="flex flex-col w-full items-center gap-2">
-                    <InputField className={`w-full ${errors.email ? 'border-red-600 placeholder-red-400' : ''}`} placeholder="Enter email" type="email" {...register('email')} />
+                    <InputField className={`w-full ${errors.email ? 'border-red-600 dark:border-red-600 placeholder-red-400' : ''}`} placeholder="Enter email" type="email" {...register('email')} />
 
                     {errors.email && <span className="text-red-600 text-sm text-start w-full">Email is required</span>}
 
-                    <InputField placeholder="Enter password" className={`w-full ${errors.password ? 'border-red-600 placeholder-red-400' : ''}`} type="password" {...register("password")} />
+                    <InputField placeholder="Enter password" className={`w-full ${errors.password ? 'border-red-600 dark:border-red-600 placeholder-red-400' : ''}`} type="password" {...register("password")} />
 
                     {errors.password && <span className="text-red-600 text-sm text-start w-full">Password must be between 6 and 20 characters</span>}
 
-                    <InputField placeholder="Enter confirm password" className={`w-full ${errors.confirmPassword ? 'border-red-600 placeholder-red-400' : ''}`} type="password" {...register("confirmPassword")} />
+                    <InputField placeholder="Enter confirm password" className={`w-full ${errors.confirmPassword ? 'border-red-600 dark:border-red-600 placeholder-red-400' : ''}`} type="password" {...register("confirmPassword")} />
                     {errors.confirmPassword && <span className="text-red-600 text-sm text-start w-full">Confirm password is required</span>}
                 </div>
 
