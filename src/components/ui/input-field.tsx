f91de4c26c9faab5920d2dcdac3de8047539f616
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-07 12:19:04 +0600

    add border primary when focusing

--- src/components/ui/input-field.tsx
+++ src/components/ui/input-field.tsx
@@ -6,10 +6,10 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
 const InputField = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
     const { className, ...rest } = props;
     const { type } = rest;
-    const btnClass = 'flex items-center justify-center gap-2 cursor-pointer select-none p-2 rounded transition duration-0 bg-primary hover:bg-primary-high text-white bg-gradient-to-br from-primary to-primary-high hover:to-primary-extra-high'
+    const btnClass = 'flex items-center justify-center gap-2 cursor-pointer select-none p-2 rounded transition duration-0 bg-primary hover:bg-primary-high text-white bg-gradient-to-br from-primary to-primary-high hover:to-primary-extra-high  active:scale-[102%] border-none'
 
     return (
-        <input {...rest} ref={ref} className={cn("p-2 border  dark:border-none outline-none active:scale-[102%] rounded dark:text-white", className, { [btnClass]: (type === 'submit') })} title="Input field" />
+        <input {...rest} ref={ref} className={cn("p-2 border  outline-none rounded dark:text-white bg-white dark:bg-black dark:border dark:border-gray-800 focus:border-primary", className, { [btnClass]: (type === 'submit') })} title="Input field" />
     );
 });
 
