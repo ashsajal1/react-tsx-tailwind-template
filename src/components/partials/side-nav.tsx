8a83dcc003de1130505fa1e0f7d1618aa155de18
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-07 10:04:05 +0600

    add animation

--- src/components/partials/side-nav.tsx
+++ src/components/partials/side-nav.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Button from '../ui/button'
 import ModeToggle from '../ui/mode-toggle'
 import Search from '../ui/search'
@@ -8,7 +9,11 @@ export default function SideNav({ isOpen, handleClose }: { isOpen: boolean, hand
     if (!isOpen) return <></>
 
     return (
-        <nav className='fixed inset-0 left-0 w-1/2 z-20 top-0 max-h-screen h-full bg-white border dark:border-gray-800 p-3 dark:bg-black'>
+        <motion.nav
+            initial={{ width: '0', opacity: '0' }}
+            animate={{width: '50%', opacity:'100'}}
+            className='fixed inset-0 left-0 w-1/2 z-20 top-0 max-h-screen h-full bg-white border dark:border-gray-800 p-3 dark:bg-black'
+        >
             {/* <div className="absolute inset-0 bg-black opacity-50"></div> */}
             <div className='flex items-center justify-between'>
                 <Text className='text-xl font-bold' label='Logo' />
@@ -24,6 +29,6 @@ export default function SideNav({ isOpen, handleClose }: { isOpen: boolean, hand
             <div className='bottom-0 absolute left-0'>
                 <ModeToggle />
             </div>
-        </nav>
+        </motion.nav>
     )
 }
