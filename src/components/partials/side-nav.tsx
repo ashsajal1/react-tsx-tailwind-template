a743c0118353223961f8b4f766354be93d791781
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-07 12:05:01 +0600

    increase width

--- src/components/partials/side-nav.tsx
+++ src/components/partials/side-nav.tsx
@@ -24,8 +24,8 @@ export default function SideNav({ isOpen, handleClose }: { isOpen: boolean, hand
                     delayChildren: 0.3,
                     staggerChildren: 0.05
                 }}
-                animate={isOpen ? { width: '50%', opacity: 1 } : { width: '0', opacity: 0 }}
-                className='fixed inset-0 left-0 z-20 top-0 max-h-full h-full bg-white border dark:border-gray-800 p-3 dark:bg-black overflow-y-auto'
+                animate={isOpen ? { width: '75%', opacity: 1 } : { width: '0', opacity: 0 }}
+                className='fixed inset-0 left-0 z-20 top-0 max-h-full h-full bg-white border dark:border-gray-800 p-3 dark:bg-black overflow-y-auto md:hidden'
 
             >
                 <div className='flex items-center justify-between'>
