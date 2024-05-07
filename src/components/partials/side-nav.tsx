8eb2d22cbdc6841acf035a36bbebf7b3b62a0472
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-05-07 12:09:44 +0600

    gray border for dark mode

--- src/components/partials/side-nav.tsx
+++ src/components/partials/side-nav.tsx
@@ -27,7 +27,7 @@ export default function SideNav({ isOpen, handleClose }: { isOpen: boolean, hand
                 animate={isOpen ? { width: '75%', opacity: 1 } : { width: '0', opacity: 0 }}
                 className='fixed inset-0 left-0 z-20 top-0 max-h-full h-full bg-white border dark:border-gray-800 px-3 dark:bg-black overflow-y-auto md:hidden'
             >
-                <div className='flex items-center justify-between h-[80px] border-b'>
+                <div className='flex items-center justify-between h-[80px] border-b dark:border-b-gray-800'>
                     <Text className='text-xl font-bold' label='Logo' />
                     <HiOutlineXMark onClick={handleClose} className='h-6 w-6 dark:text-white text-black cursor-pointer' />
                 </div>
