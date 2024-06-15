232a58238368aaef63c41b1dfbe7f01205a33322
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-15 18:52:19 +0600

    feat: add transition duration

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ export default function Button({ children, variant = "solid", ...props }: PropsW
 
     const { className } = props;
 
-    const buttonStyles = "flex items-center justify-center gap-2 cursor-pointer select-none p-2 rounded transition duration-0 transform active:scale-[102%]";
+    const buttonStyles = "flex items-center justify-center gap-2 cursor-pointer select-none p-2 rounded transition duration-300 transform active:scale-[102%]";
     const ghostStyle = "bg-transparent text-gray-950 hover:bg-slate-100 hover:text-gray-900 dark:hover:bg-gray-900"
     const outlineStyle = "border border-primary dark:border-gray-800 text-primary hover:bg-primary hover:text-white"
     const dangerStyle = 'bg-red-600 hover:bg-red-700';
