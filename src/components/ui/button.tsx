290bc8544c0a59d67b4d1dded687fd8c528150ec
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-15 18:51:10 +0600

    style: remove primary color from ghost btn

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ export default function Button({ children, variant = "solid", ...props }: PropsW
     const { className } = props;
 
     const buttonStyles = "flex items-center justify-center gap-2 cursor-pointer select-none p-2 rounded transition duration-0 transform active:scale-[102%]";
-    const ghostStyle = "bg-transparent text-primary hover:bg-primary hover:text-white"
+    const ghostStyle = "bg-transparent text-gray-950 hover:bg-slate-100 hover:text-gray-900 dark:hover:bg-gray-900"
     const outlineStyle = "border border-primary dark:border-gray-800 text-primary hover:bg-primary hover:text-white"
     const dangerStyle = 'bg-red-600 hover:bg-red-700';
     const dangerOutlineStyle = 'border bg-white dark:bg-black border-red-600 text-red-600 hover:bg-red-200 hover:text-red';
