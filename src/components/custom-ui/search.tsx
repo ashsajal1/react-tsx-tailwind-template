7f5b76d991e068b031d7b1674bc8946b7d338f03
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-16 12:59:10 +0600

    style: remove bg dark

--- src/components/custom-ui/search.tsx
+++ src/components/custom-ui/search.tsx
@@ -1,11 +1,12 @@
 import { CiSearch } from 'react-icons/ci'
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
+import { cn } from '@/lib/utils'
 
 export default function Search() {
   return (
     <div className='flex items-center justify-between border rounded focus-within:ring-1 focus-within:ring-primary px-1 dark:border-gray-800 bg-white dark:bg-black'>
-        <Input type="text" className='focus:ring-0 border-none focus:placeholder:text-primary dark:bg-black' placeholder='Search' />
+        <Input type="text" className={cn('border-none focus:outline-none focus:placeholder:text-primary')} placeholder='Search' />
         <Button className='h-full'><CiSearch /></Button>
     </div>
   )
