1dcab2ffa22d518a52341201746ed87d85cd49d4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-16 12:55:29 +0600

    style: set padding 1

--- src/components/custom-ui/search.tsx
+++ src/components/custom-ui/search.tsx
@@ -4,7 +4,7 @@ import { Input } from '../ui/input'
 
 export default function Search() {
   return (
-    <div className='flex items-center justify-between border rounded focus-within:ring-1 focus-within:ring-primary px-2 dark:border-gray-800 bg-white dark:bg-black'>
+    <div className='flex items-center justify-between border rounded focus-within:ring-1 focus-within:ring-primary px-1 dark:border-gray-800 bg-white dark:bg-black'>
         <Input type="text" className='focus:ring-0 border-none focus:placeholder:text-primary dark:bg-black' placeholder='Search' />
         <Button className='h-full'><CiSearch /></Button>
     </div>
