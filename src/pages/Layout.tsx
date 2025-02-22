5adab6ab8053b80c3c8b7ad38eec2c4303adf0e8
Ashsajal <ashsajal1@gmail.com>
2025-02-22 11:17:09 +0600

    style: layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,15 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/partials/navbar'
-import Footer from '../components/partials/footer'
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/partials/navbar";
+import Footer from "../components/partials/footer";
+
 export default function RootLayout() {
-    return (
-        <>
-            <Navbar />
-            <main className='p-2 mt-[80px]'>
-                <Outlet />
-            </main>
-            <Footer />
-        </>
-    )
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow p-2 mt-[80px]">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
