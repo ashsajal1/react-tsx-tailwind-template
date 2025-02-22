e67136451cf215bffbc2968606a1c5f880824387
Ashsajal <ashsajal1@gmail.com>
2025-02-22 11:19:28 +0600

    style: use default design

--- src/components/custom-ui/search.tsx
+++ src/components/custom-ui/search.tsx
@@ -1,13 +1,12 @@
 import { CiSearch } from 'react-icons/ci'
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
-import { cn } from '@/lib/utils'
 
 export default function Search() {
   return (
-    <div className='flex items-center justify-between border rounded focus-within:ring-1 focus-within:ring-primary px-1 dark:border-gray-800 bg-white dark:bg-black'>
-        <Input type="text" className={cn('border-none focus:outline-none focus:placeholder:text-primary')} placeholder='Search' />
-        <Button className='h-full'><CiSearch /></Button>
+    <div className='flex items-center justify-between gap-2'>
+        <Input type="text" placeholder='Search' />
+        <Button variant={'secondary'}><CiSearch /></Button>
     </div>
   )
 }
