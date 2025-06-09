da23eb2ab4cdee31552b92e3a231b19c25c502bb
Ashsajal <ashsajal1@gmail.com>
2025-06-09 09:23:30 +0600

    ✨ feat: integrate Redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { HelmetProvider } from 'react-helmet-async'
+import { store } from './lib/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <App />
-      </ThemeProvider>
-    </HelmetProvider>
+    <Provider store={store}>
+      <HelmetProvider>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <App />
+        </ThemeProvider>
+      </HelmetProvider>
+    </Provider>
   </React.StrictMode>,
 )
