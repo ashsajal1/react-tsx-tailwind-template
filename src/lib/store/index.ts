47621893d589ada04e65ac46051f870ab500034b
Ashsajal <ashsajal1@gmail.com>
2025-06-09 09:25:21 +0600

    ✨ feat: add redux-persist

--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -1,20 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userPreferencesReducer from './slices/userPreferencesSlice';
 import authReducer from './slices/authSlice';
 import uiReducer from './slices/uiSlice';
 
+const rootReducer = combineReducers({
+  userPreferences: userPreferencesReducer,
+  auth: authReducer,
+  ui: uiReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  // Only persist these reducers
+  whitelist: ['userPreferences', 'auth'],
+  // Don't persist these reducers
+  blacklist: ['ui'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    userPreferences: userPreferencesReducer,
-    auth: authReducer,
-    ui: uiReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { HelmetProvider } from 'react-helmet-async'
-import { store } from './lib/store'
+import { store, persistor } from './lib/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HelmetProvider>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <App />
-        </ThemeProvider>
-      </HelmetProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <HelmetProvider>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <App />
+          </ThemeProvider>
+        </HelmetProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
